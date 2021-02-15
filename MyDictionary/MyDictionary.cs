4cd0a6b36ad00a99f22a649fdc98be274c282855
Edvin DAVULCU <edvindavulcu@gmail.com>
2021-02-15 20:41:23 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<kT, vT>
+    {
+        kT[] names;
+        vT[] ages;
+        public MyDictionary()
+        {
+            names = new kT[0];
+            ages = new vT[0];
+        }
+        public void Add(kT name , vT age)
+        {
+            kT[] tempArray1 = names;
+            vT[] tempArray2 = ages;
+
+            names = new kT[names.Length + 1];
+            ages = new vT[ages.Length + 1];
+
+            for (int i = 0; i < tempArray1.Length; i++)
+            {
+                names[i] = tempArray1[i];
+            }
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                ages[i] = tempArray2[i];
+            }
+            names[names.Length - 1] = name;
+            ages[ages.Length - 1] = age;
+        }
+        public void Read()
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i] + " " + ages[i]);
+            }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+            myDictionary.Add("Amy" , 36);
+            myDictionary.Add("Jack", 45);
+            myDictionary.Add("Cole", 27);
+            myDictionary.Read();
+
+
+        }
+    }
+}
